24de73290eaea5c70e06b8a586ca21f15156ef61
Mārcis Jurkons <marcis.jurkons@hotmail.com>
2024-04-22 07:49:48 +0300

    Some changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,5 +1,6 @@
 # Uncomment the imports before you add the code
 from django.urls import path
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
@@ -16,7 +17,7 @@ urlpatterns = [
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),    
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
 
