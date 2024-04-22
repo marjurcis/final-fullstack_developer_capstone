2f5734128c6da16f0865da16b5c21085f3192df7
Mārcis Jurkons <marcis.jurkons@hotmail.com>
2024-04-22 13:21:48 +0300

    Some changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -121,7 +121,7 @@ def add_review(request):
     if(request.user.is_anonymous == False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status":401, "message": "Error in posting review"})
