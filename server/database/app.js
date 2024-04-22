a886b9d6ced710d30b2356aa0d617191f1929c64
Mārcis Jurkons <marcis.jurkons@hotmail.com>
2024-04-22 13:46:57 +0300

    Some changes

--- server/database/app.js
+++ server/database/app.js
@@ -1,12 +1,13 @@
 /*jshint esversion: 8 */
+/*jshint sub: true*/
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const cors = require('cors')
-const app = express()
+const cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -33,7 +34,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-  res.send("Welcome to the Mongoose API")
+  res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -69,8 +70,8 @@ app.get('/fetchDealers', async (req, res) => {
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
   try {
-    const documents = await Dealerships.find({ state: req.params.state })
-    res.json(documents)
+    const documents = await Dealerships.find({ state: req.params.state });
+    res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
   }
@@ -79,8 +80,8 @@ app.get('/fetchDealers/:state', async (req, res) => {
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
   try {
-    const documents = await Dealerships.find({ id: req.params.id })
-    res.json(documents)
+    const documents = await Dealerships.find({ id: req.params.id });
+    res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
   }
@@ -89,8 +90,8 @@ app.get('/fetchDealer/:id', async (req, res) => {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort({ id: -1 })
-  let new_id = documents[0]['id'] + 1
+  const documents = await Reviews.find().sort({ id: -1 });
+  let new_id = documents[0]['id'] + 1;
 
   const review = new Reviews({
     "id": new_id,
